2d0bc03f35f30d7c117a4cca6d0429065bcbb23b
maarish441-crypto <maarish441@gmail.com>
2024-08-12 21:54:24 +0530

    first commit

--- controllers/productCotroller.js
+++ controllers/productCotroller.js
@@ -3,6 +3,7 @@ const Product = require("../models/product");
 const allProducts = async (req, res) => {
   try {
     let products = await Product.find({});
+    // console.log(products + "hey");
     res.render("products/index", { products });
   } catch (e) {
     res.status(500).render("error", { err: e.message });
